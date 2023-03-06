f2d400769312955d78039b9ef0c38607ea074afe
Jaime M <jaimemarsh19@gmail.com>
2023-03-05 21:09:34 -0500

    starting search history

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+var apiKey = "c33d7e9c5e7e06d80507552604aaf5a5"
+
+//html references
+var searchBox = document.querySelector('#searchBox')
+var searchBar = document.querySelector('#searchInput')
+var todayBox = document.querySelector('#todayBox')
+var forecastBox = document.querySelector('#forecastBox')
+var searchHistory = document.querySelector('#searchHistory')
+
+// display search history list
