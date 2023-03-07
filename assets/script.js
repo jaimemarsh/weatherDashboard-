594734b14a7af32e90f1ea40216992901af98418
Jaime M <jaimemarsh19@gmail.com>
2023-03-07 18:13:13 -0500

    starting JS, works

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,14 @@
 let weatherApiKey = "c33d7e9c5e7e06d80507552604aaf5a5";
+let searchInput = $("#searchInput")
+
+searchButton.addEventListener("click", editSearchInput)
+
+function editSearchInput() {
+    let errorAlert = document.getElementById("divclass")
+    //Sets search value to variable & removes the empty spaces between characters
+    let city = searchInput.val().replace(/\s/g, "");
+    if (city == "") {
+        errorAlert.querySelector("#error").innerHTML = "Enter a city and state to see the weather!"
+    }
+}
+
