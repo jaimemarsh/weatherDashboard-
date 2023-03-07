8ca85b96f6d3e8359fd1d3f33edfcdf3dffd741e
Jaime M <jaimemarsh19@gmail.com>
2023-03-07 17:48:56 -0500

    Not functional but looks correct

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,1 @@
-// global var
-// weather api
-var apiKey = "c33d7e9c5e7e06d80507552604aaf5a5";
-// need to look over, need to convert lat and lon to city for api
-var geoCoord = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}";
-var searchHistory = []
-
-//html references
-var searchBox = document.querySelector('#searchBox')
-var searchBar = document.querySelector('#searchInput')
-var todayBox = document.querySelector('#todayBox')
-var forecastBox = document.querySelector('#forecastBox')
-var searchHistoryBox = document.querySelector('#searchHistory')
-
-
+let weatherApiKey = "c33d7e9c5e7e06d80507552604aaf5a5";
